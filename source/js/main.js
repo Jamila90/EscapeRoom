383c22c7caf370404b848bf2c8f6cab98db32b21
Jamila90 <jamila.shakurova@gmail.com>
2020-09-15 11:17:53 +0400

    Стилизовала страницы 1.1

--- source/js/main.js
+++ source/js/main.js
@@ -9,7 +9,8 @@ let linkCity = document.querySelector('.main-header__link');
 let modalCity = document.querySelector('.modal');
 let questionModal = document.querySelector('.modal-form');
 let modalToggle = document.querySelector('.modal__toggle');
-let questionToggle = document.querySelector('.footer-page__button');
+let questionClose = document.querySelector('.modal-form__toggle');
+let questionBtn = document.querySelector('.footer-page__button');
 
 mainHeader.classList.remove('main-header--nojs');
 mainNav.classList.remove('nav--nojs');
@@ -35,10 +36,51 @@ modalToggle.addEventListener('click', function () {
   modalCity.classList.remove('modal--show');
 });
 
-questionToggle.addEventListener('click', function () {
+questionBtn.addEventListener('click', function () {
   questionModal.classList.add('modal-form--show');
 });
 
-questionToggle.addEventListener('click', function () {
+questionClose.addEventListener('click', function () {
   questionModal.classList.remove('modal-form--show');
+});
+
+$(document).ready(function () {
+  $('.modal-form__item input').on('input', function (e) {
+    let $this = $(e.target);
+    if ($this.val() === '') {
+      $this.removeClass('modal-form__item-filled');
+    } else {
+      $this.addClass('modal-form__item-filled');
+    }
+  });
+
+  $('.modal-form__question').validate({
+    rules: {
+      name: true,
+      minlength: 2
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    messages: {
+      email: {
+        required: 'Пожалуйста заполните поле',
+        email: 'Введён некорректный e-mail, попробуйте заново'
+      },
+      name: {
+        required: 'Пожалуйста заполните поле',
+        minlength: 'Введите не менее 2х символов'
+      }
+    }
+  });
+
+  $('.modal-form__label').on('submit', function () {
+    let submit = document.querySelector('.modal-form__button');
+    if (document.querySelector('.modal-form__label').checked) {
+      submit.disabled = '';
+    } else {
+      submit.disabled = 'disabled';
+    }
+  });
 });
